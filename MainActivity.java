84e0b9972e7605f104437fe8beef1a839993a833
BestaTechnology <32355433+BestaTechnology@users.noreply.github.com>
2019-01-27 02:49:10 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,37 @@
+package voice_to_text.sandy148101.com.phonecall;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView =(TextView)findViewById(R.id.textView);
+        imageView =(ImageView)findViewById(R.id.imageview);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:"));
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
+}
